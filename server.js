db8a557e6871e2f4060f2ca847e0ef349ae612c9
Mel Maki <melody.rackham@gmail.com>
2019-12-06 10:35:46 -0700

    GET Requests for projects, along with middleware

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const server = express();
 
+// ROUTES
+const actionRouter = require('./data/helpers/actionRouter');
+const projectRouter = require('./data/helpers/projectRouter');
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalURL} at ${new Date()}`);
+  next();
+}
+
+server.use(logger);
 server.use(express.json());
+
 server.get('/', (req, res) => {
-  const queryParameters = req.query;
-  res.status(200).json(queryParameters);
+  res.send(`<h1> Working! </h1>`);
 });
 
 module.exports = server;
